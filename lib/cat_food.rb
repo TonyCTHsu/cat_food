18ae43181cb6f2bd2220b1cd180c0ef9c195d2d9
Tony Hsu <tonyc.t.hsu@gmail.com>
2024-05-03 21:59:38 +0200

    git init

---
+++ lib/cat_food.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "cat_food/version"
+
+module CatFood
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/cat_food_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CatFood do
+  it "has a version number" do
+    expect(CatFood::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
